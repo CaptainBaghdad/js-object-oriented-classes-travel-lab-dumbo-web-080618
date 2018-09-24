24cf99f131b8f3acf83e065420b1e8b913ee8b90
Jason Pitts <juniorjr083@gmail.com>
2018-09-24 17:40:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date (startDate);
+    
+  }
+  
+ yearsExperienceFromBeginningOf(endDate){
+   
 
+ return parseInt(endDate) -  parseInt(this.startDate.getFullYear()) -1;
+  
+   
+ }
+  
+}
+
+class Route {
+  constructor(start, end){
+    this.start = start;
+    this.end = end;
+    
+    console.log(typeof end);
+  } 
+ 
+ 
+ blocksTravelled(){
+   let e ;
+   let s;
+   
+  for(let key in this.end){
+   e = key.vertical;
+   console.log(e);
+    
+  }
+  
+ }
+ 
+}
